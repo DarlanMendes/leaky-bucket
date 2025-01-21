b794abe65ebcea7fb16f7214e48562ff48b11057
darlan-mendes <dedson.darlan@irede.org.br>
2025-01-21 13:59:27 -0300

    refactoring leaky bucket

--- api/routes/middlewares/ratelimiter.go
+++ api/routes/middlewares/ratelimiter.go
@@ -15,26 +15,29 @@ const bucket_size = 10 // Represent volume bucket capacity
 
 var l_time = time.Now() // It's used to measure elapsed time
 
-const tokenPerPeriod = 6 // Represent quantity throttle allows per second
+const tokenPerPeriod = 4 // Represent quantity throttle allows per second
 
 func LeakyBucket(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
+
 		//Verify if bucket is full
 		tm := time.Now()
 		elapsed := tm.Sub(l_time)
 		l_time = tm
 		var consumes = elapsed.Seconds() * tokenPerPeriod
+		fmt.Println("Token consumed by request ", consumes)
 		fmt.Println("Token stored ", stored)
 		stored = math.Max(0, stored-consumes)
 		fmt.Println("Elapsed time ", elapsed)
-		if stored < bucket_size {
+		if (stored <= bucket_size) && (consumes <= tokenPerPeriod) {
 			fmt.Println("Accept with tokens", stored)
 			stored++
+			return next(c)
 
 		} else {
 			fmt.Println("Denied 429")
 			return c.JSON(http.StatusTooManyRequests, 429)
 		}
-		return next(c)
+
 	}
 }
